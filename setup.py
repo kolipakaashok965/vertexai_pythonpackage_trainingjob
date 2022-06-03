6e31ebdefc866f125569596c797ad336fe182626
kolipakaashok965 <kolipakaashok965@users.noreply.github.com>
2022-06-03 18:04:50 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = ['gcsfs','pandas'
+]
+
+setup(
+    name='HousePrice',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    description='House price prediction',
+    requires=[]
+)
